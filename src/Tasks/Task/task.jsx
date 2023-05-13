3a1dbee5cdc938337f22be37d4cb4f861035a262
Admin <syubebayevm@gmail.com>
2023-05-13 14:51:55 +0500

    first commit

--- src/Tasks/Task/task.jsx
+++ src/Tasks/Task/task.jsx
@@ -1,40 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { EMPTY_CHECKBOX, MENU } from '../../images'
+import './index.css'
 
 export default function Task() {
 
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       id: 0,
       content: 'Write Essay',
-      status: 'todo'
+      status: 'todo',
+      menu: MENU,
+      chekbox: EMPTY_CHECKBOX, 
     },
     {
       id: 1,
       content: 'One Hour CSS Course Online',
-      status: 'todo'
+      status: 'todo',
+      menu: MENU,
+      chekbox: EMPTY_CHECKBOX, 
     },
     {
       id: 2,
       content: 'Buy One Way Tickets to San Fransico',
-      status: 'todo'
+      status: 'todo',
+      menu: MENU,
+      chekbox: EMPTY_CHECKBOX, 
     },
     {
       id: 3,
       content: 'Go to Gym',
-      status: 'todo'
+      status: 'todo',
+      menu: MENU,
+      chekbox: EMPTY_CHECKBOX, 
     },
     {
       id: 4,
       content: 'Buy Groceries',
-      status: 'todo'
+      status: 'todo',
+      menu: MENU,
+      chekbox: EMPTY_CHECKBOX, 
     }
-  ]
+  ])
 
 
-  return (
-    tasks.map((task)=>{
-      <button><img src={MENU} alt="" /></button>
-    })
-  )
+  return tasks.map((task)=>(
+    <div className='list-of-tasks'>
+      <button className='menu'><img src={task.menu} alt="menu"/></button>
+      <button className='checkbox'><img src={task.chekbox} alt="chekbox"/></button>
+      <p>{task.content}</p>
+    </div>
+  ))
 }
--- src/Tasks/tasks.jsx
+++ src/Tasks/tasks.jsx
@@ -3,6 +3,8 @@ import Task from './Task/task'
 import './index.css'
 
 export default function Tasks() {
+
+  
   return (
     <div style={{display: 'flex', flexDirection: 'column', padding: '64px 90px', gap: '24px'}}>
         <div class = 'to-do-header'>To Do</div>
